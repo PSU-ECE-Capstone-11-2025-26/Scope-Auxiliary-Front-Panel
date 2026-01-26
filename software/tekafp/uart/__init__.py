7ef5c1bdec127467589ebb725d7ba0e2fa6dba4f
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2026-01-26 14:29:51 -0800

    fix: UART message should be bytes

--- software/tekafp/uart/__init__.py
+++ software/tekafp/uart/__init__.py
@@ -32,7 +32,7 @@ class UARTBridge:
         line: bytes = self.serial.readline()
         self.queue.put(line)
 
-    def _write(self, msg: str) -> None:
+    def _write(self, msg: bytes) -> None:
         if not self.serial or not self.serial.is_open:
             return
         self.serial.write(msg)
