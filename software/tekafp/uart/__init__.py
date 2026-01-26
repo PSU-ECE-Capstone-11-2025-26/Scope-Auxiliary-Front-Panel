3f850bd9edfa84426229265f81e39919a432d5b0
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2026-01-25 21:28:42 -0800

    feat: enable UART connection

---
+++ software/tekafp/uart/__init__.py
@@ -0,0 +1,33 @@
+from queue import Queue
+
+import serial
+
+
+class UARTBridge:
+    """
+    A class to represent the UART bridge between the interface processor and the main
+    processor.
+    """
+    def __init__(self, port: str, baudrate: int = 9600) -> None:
+        self.queue: Queue[bytes] = Queue()
+        self.port: str = port
+        self.baudrate: int = baudrate
+        self.serial = None
+
+    def connect(self) -> bool:
+        """
+        Initialize a connection with the UART interface.
+        :return: True if the connection was successful, False otherwise.
+        """
+        self.serial = serial.Serial(self.port, self.baudrate)
+        return True
+
+    def read(self) -> None:
+        """
+        Read data from the UART.
+        The raw bytes received are added to the queue attribute.
+        """
+        if not self.serial or not self.serial.is_open:
+            return
+        line: bytes = self.serial.readline()
+        self.queue.put(line)
