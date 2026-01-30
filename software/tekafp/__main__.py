ad144ef426e7ede91bfcd40afd5a5dd6580ba08f
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2026-01-29 22:22:26 -0800

    feat: UART init and decoding in main

--- software/tekafp/__main__.py
+++ software/tekafp/__main__.py
@@ -1,2 +1,21 @@
+from uart import UARTBridge
+
+from tekafp.input import Input
+
+
+def main() -> None:
+    bridge = UARTBridge("/dev/serial0", 115200)
+    bridge.connect()
+
+    # main loop
+    while True:
+        bridge.read()
+        if bridge.queue.empty():
+            continue
+        else:
+            afp_input = Input.from_bytes(bridge.queue.get())
+            # TODO: translate afp_input to SCPI and send it
+
+
 if __name__ == "__main__":
-    print("tek-afp")
+    main()
