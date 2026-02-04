00f1acf2f5abbee09225f862534a58aba0d135a6
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2026-02-04 11:17:19 -0800

    test: update uart unit test to run using loopback
    Use loopback (loop://) url so that a physical testbench is no longer required. This test should run on any system which supports pyserial.

--- software/test/uart/test_uart.py
+++ software/test/uart/test_uart.py
@@ -1,18 +1,20 @@
-from os import uname
-
-import pytest
+import serial
 
 from tekafp.uart import UARTBridge
 
 
-@pytest.mark.skipif(uname().nodename != "tek-afp",
-                    reason="Hostname != tek-afp: test must run on a testbench")
 def test_uart_read() -> None:
-    # Requires UART set up on the Pi and the pins to be connected in loopback
-    # GPIO 14 connected to GPIO 15 (UART0)
     msg: bytes = b'testing123\n'
 
     bridge = UARTBridge("/dev/serial0", timeout=5, write_timeout=5)
+    # overwrite the default serial with a loopback url
+    bridge.serial = serial.serial_for_url(
+        "loop://",
+        baudrate=115200,
+        do_not_open=True,
+        timeout=bridge.timeout,
+        write_timeout=bridge.write_timeout
+    )
     bridge.connect()
     write_status = bridge._write(msg)
     assert write_status
