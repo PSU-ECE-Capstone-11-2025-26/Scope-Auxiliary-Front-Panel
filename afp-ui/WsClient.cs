d8764f6a669aa98680e0a1fbb732076eb1ee9e18
Andrew Stanton <81195290+sneakyaardvark@users.noreply.github.com>
2026-04-09 20:04:05 -0700

    feat: handle websocket state transitions separately

--- afp-ui/WsClient.cs
+++ afp-ui/WsClient.cs
@@ -15,6 +15,7 @@ public partial class WsClient : Node
 	private readonly Queue<IPacketData> _sendQueue = new();
 
 	private WebSocketPeer _socket;
+	private WebSocketPeer.State _prevState = WebSocketPeer.State.Closed;
 
 	private readonly JsonSerializerOptions _options = new JsonSerializerOptions
 	{
@@ -73,14 +74,34 @@ public partial class WsClient : Node
 	public override void _Process(double delta)
 	{
 		_socket.Poll();
-
 		WebSocketPeer.State state = _socket.GetReadyState();
 
+		// on state transitions
+		if (state != _prevState)
+		{
+			switch (state)
+			{
+				case WebSocketPeer.State.Connecting:
+					GD.Print("WebSocket: connecting...");
+					break;
+				case WebSocketPeer.State.Open:
+					GD.Print("WebSocket: connected");
+					break;
+				case WebSocketPeer.State.Closing:
+					GD.Print("WebSocket: closing...");
+					break;
+				case WebSocketPeer.State.Closed:
+					SetProcess(false);
+					int code = _socket.GetCloseCode();
+					GD.Print($"WebSocket: closed with code {code}. Clean: {code != -1}");
+					break;
+			}
+			_prevState = state;
+		}
+
+		// every tick
 		switch (state)
 		{
-			case WebSocketPeer.State.Connecting:
-				GD.Print("WebSocket: still trying to connect");
-				break;
 			case WebSocketPeer.State.Open:
 				while (_socket.GetAvailablePacketCount() > 0)
 				{
@@ -91,7 +112,9 @@ public partial class WsClient : Node
 					{
 						var packetObj = JsonSerializer.Deserialize<PacketContainer>(packetText, _options);
 						if (packetObj != null)
+						{
 							ReceiveQueue.Enqueue(packetObj);
+						}
 						else
 						{
 							GD.PushWarning($"WebSocket: null from packet {packetText}");
@@ -101,18 +124,14 @@ public partial class WsClient : Node
 					{
 						GD.PushError($"WebSocket: failed to deserialize: {e.Message}");
 					}
-					
 				}
+
 				SendAllPacketData();
 
 				break;
+			case WebSocketPeer.State.Connecting:
 			case WebSocketPeer.State.Closing:
-				GD.Print("WebSocket: closing...");
-				break;
 			case WebSocketPeer.State.Closed:
-				int code = _socket.GetCloseCode();
-				GD.Print($"WebSocket: closed with code {code}. Clean: {code != -1}");
-				SetProcess(false);
 				break;
 			default:
 				GD.PushError("WebSocket: unknown state");
