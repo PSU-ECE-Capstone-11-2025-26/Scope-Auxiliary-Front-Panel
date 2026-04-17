926891271d676622346abdcca35e229e71be492d
Antonio Hernandez Olivares <ahernandez3993@gmail.com>
2026-04-16 20:05:04 -0700

    replaced queued uart writes with synchronous write for LED updates to eliminate latency from blocking read loop. this thing is now FAST

--- software/tekafp/__main__.py
+++ software/tekafp/__main__.py
@@ -189,7 +189,8 @@ class Controller:
         ]
 
         msg = f"IV{channel}0_R:{1 if state else 0}\n".encode("utf-8")
-        self.bridge.queue_write(msg)
+        #self.bridge.queue_write(msg)
+        self.bridge.write_sync(msg)
         print(f"[UART->PICO] {msg.decode().strip()}")
 
         #for msg in msgs:
