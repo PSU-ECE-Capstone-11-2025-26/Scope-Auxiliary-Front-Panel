2c5263849e516b8713e78cf0562de4832a0f47ac
Andrew Stanton <81195290+sneakyaardvark@users.noreply.github.com>
2026-04-25 22:35:55 -0700

    feat: debug mode config option

--- afp-ui/Config.cs
+++ afp-ui/Config.cs
@@ -4,4 +4,5 @@ public class Config
 {
     public string WebSocketUrl { get; set; } = "ws://127.0.0.1:8000/ws";
     public bool SaveSelectedScopes { get; set; } = true;
+    public bool DebugMode { get; set; }
 }
